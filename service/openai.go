bc6460308038975fe3c843f7755ee33918d9619f
akane9506 <akane19950611@gmail.com>
2025-08-16 18:36:49 -0700

    dictionary response and parse completed

--- service/openai.go
+++ service/openai.go
@@ -1,6 +1,13 @@
 package service
 
-import "github.com/openai/openai-go/v2"
+import (
+	"context"
+	"encoding/json"
+	"reflect"
+
+	"github.com/jiten-mobile/service/graph/model"
+	"github.com/openai/openai-go/v2"
+)
 
 type LLMService struct {
 	client *openai.Client
@@ -10,6 +17,35 @@ func NewLLMService(client *openai.Client) *LLMService {
 	return &LLMService{client: client}
 }
 
-func (llm *LLMService) StructuredOutput(prompt string, model interface{}) {
+func (llm *LLMService) StructuredOutput(word string, systemPrompt string, jsonDescription string) (*model.Word, error) {
 
+	jsonSchema := openai.ResponseFormatJSONSchemaJSONSchemaParam{
+		Name:        "definition_generation",
+		Description: openai.String(jsonDescription),
+		Schema:      JsonTypeOf(reflect.TypeOf(model.Word{})),
+	}
+
+	resp, err := llm.client.Chat.Completions.New(
+		context.Background(),
+		openai.ChatCompletionNewParams{
+			Messages: []openai.ChatCompletionMessageParamUnion{
+				openai.SystemMessage(systemPrompt),
+				openai.UserMessage(word),
+			},
+			ResponseFormat: openai.ChatCompletionNewParamsResponseFormatUnion{
+				OfJSONSchema: &openai.ResponseFormatJSONSchemaParam{JSONSchema: jsonSchema},
+			},
+			Model: openai.ChatModelGPT5Nano,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var data model.Word
+	if err := json.Unmarshal([]byte(resp.Choices[0].Message.Content), &data); err != nil {
+		return nil, err
+	}
+	return &data, nil
 }
